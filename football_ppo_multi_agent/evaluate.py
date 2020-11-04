56138a0fe6b61dacd413068ff0ae06aac20ed2ce
sirakik <shiraki212@gmail.com>
2020-11-04 20:26:10 +0900

    evaluate

--- football_ppo_multi_agent/evaluate.py
+++ football_ppo_multi_agent/evaluate.py
@@ -1,49 +1,83 @@
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import cv2
+import datetime
+import numpy as np
+
 import torch
 import gym
 import gfootball
 
-from football_ppo.tools.actor_critic import ActorCritic
-from football_ppo.tools.utils import make_env, convert_tensor_obs
-from multiprocessing_env import DummyVecEnv, VecNormalize
+
+from tools.actor_critic import ActorCritic
+from tools.utils import make_env, convert_tensor_obs
+
+def modify_obs(obs):
+    obs = obs[0] # (agents, 115) -> (115)
+    obs[97:108] = [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.]
+    return obs
 
 
 MODEL_NAME = 'mlp'
-LOAD_PATH = 'log/gomi/john_due.pt'
 
+OUTPUT_DIR = 'log/gomi'
+LOAD_PATH = 'log/gomi/model_john.pt'
 
 # environment
 ENV_NAME = '11_vs_11_stochastic'
 REPRESENTATION = 'simple115v2'
 REWARDS = 'scoring,checkpoints'
-LEFT_AGENT = 1
+LEFT_AGENT = 11
 RIGHT_AGENT = 0
-env = make_env(ENV_NAME, REPRESENTATION, REWARDS, LEFT_AGENT, RIGHT_AGENT)
-env = DummyVecEnv([env])
-obs_shape = envs.observation_space.low.shape[0]
-action_space = envs.action_space.n
-current_obs = torch.zeros(1, *obs_shape)
+env = make_env(ENV_NAME, REPRESENTATION, REWARDS, LEFT_AGENT, RIGHT_AGENT, iprocess=1)
+obs_shape = env.observation_space.shape[1]
+action_space = env.action_space.nvec[0]
+current_obs = torch.zeros(1, obs_shape)
 
 # model
-model = ActorCritic(obs_shape, action_space, MODEL_NAME)
+model = ActorCritic(obs_shape, action_space, MODEL_NAME, LEFT_AGENT)
 model.load_state_dict(torch.load(LOAD_PATH))
 
+img = []
+episode_reward = 0.0
+
 print('# start! :)')
+obs = env.reset()
+obs = modify_obs(obs)
+current_obs = convert_tensor_obs(obs, current_obs)
 for i in range(3000):
-    obs = env.reset()
+    with torch.no_grad():
+        _, action, _ = model.action(current_obs)
+
+    action = action.transpose(1, 0).squeeze()
+
+    obs, reward, _, _ = env.step(action.numpy())
+
+    episode_reward += reward.mean()
+
+    obs = modify_obs(obs)  # add
     current_obs = convert_tensor_obs(obs, current_obs)
-    done = False
-    episode_reward = 0.0
-    while not done:
-        with torch.no_grad():
-            _, action, _ = model.action(current_obs)
 
-        action = action.unsqueeze(1)
+    # render
+    bgr = env.render('rgb_array')
+    img.append(cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)) # BGR -> RGB
 
-        obs, reward, done, _ = env.step(action.numpy())
+print('# Episode reward: {}'.format(episode_reward))
 
-        episode_reward += reward
 
-        current_obs = convert_tensor_obs(obs, current_obs)
-        env.render()
+print('# saving video. . .')
+dpi = 72
+interval = 50 # ms
+plt.figure(figsize=(img[0].shape[1]/dpi,img[0].shape[0]/dpi),dpi=dpi)
+plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
+patch = plt.imshow(img[0])
+plt.axis=('off')
+animate = lambda i: patch.set_data(img[i])
+ani = animation.FuncAnimation(plt.gcf(),animate,frames=len(img),interval=interval)
+now = datetime.datetime.now()
+filename = OUTPUT_DIR + '/' + now.strftime('%Y%m%d_%H%M%S') + '.mp4'
+ani.save(filename, writer="ffmpeg")
+plt.close()
+print('# done. output: -> [{}]'.format(filename))
 
-    print('# Episode reward: {}'.format(episode_reward))
+env.close()
